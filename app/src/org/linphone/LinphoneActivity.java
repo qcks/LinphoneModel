bef3a41dcb66b9dc4bed49a8e2e05f3107d84b41
QCKS <2397747783@qq.com>
2017-11-13 18:09:40 +0800

    收到来电

--- app/src/org/linphone/LinphoneActivity.java
+++ app/src/org/linphone/LinphoneActivity.java
@@ -1,6 +1,7 @@
 package org.linphone;
 
 import android.app.Dialog;
+import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
@@ -10,17 +11,28 @@ import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import org.linphone.Utils.AppLog;
 import org.linphone.core.LinphoneAccountCreator;
 import org.linphone.core.LinphoneAddress;
+import org.linphone.core.LinphoneAuthInfo;
+import org.linphone.core.LinphoneCall;
+import org.linphone.core.LinphoneCallParams;
+import org.linphone.core.LinphoneChatMessage;
+import org.linphone.core.LinphoneChatRoom;
+import org.linphone.core.LinphoneCore;
 import org.linphone.core.LinphoneCoreException;
 import org.linphone.core.LinphoneCoreFactory;
 import org.linphone.core.LinphoneCoreListenerBase;
+import org.linphone.core.LinphoneProxyConfig;
 import org.linphone.mediastream.Log;
+
+import java.util.List;
 
 /**
  * Created by qckiss on 2017/8/18.
@@ -32,6 +44,10 @@ public class LinphoneActivity extends AppCompatActivity{
     private LinphoneCoreListenerBase mListener;
     private LinphoneAddress address;
     private LinphoneAccountCreator accountCreator;
+    private boolean alreadyAcceptedOrDeniedCall;
+    private LinphoneCall mCall;
+
+    private TextView textView;
     static final boolean isInstanciated() {
         return instance != null;
     }
@@ -47,7 +63,6 @@ public class LinphoneActivity extends AppCompatActivity{
 
 //        TextView toastText = (TextView) layout.findViewById(R.id.toastMessage);
 //        toastText.setText(message);
-
         final Toast toast = new Toast(getApplicationContext());
         toast.setGravity(Gravity.CENTER, 0, 0);
         toast.setDuration(duration);
@@ -80,54 +95,103 @@ public class LinphoneActivity extends AppCompatActivity{
         accountCreator.setDomain(getResources().getString(R.string.default_domain));
 //        accountCreator.setListener(this);
         initLogin();
-//        mListener = new LinphoneCoreListenerBase() {
-//
-//            @Override
-//            public void configuringStatus(LinphoneCore lc, final LinphoneCore.RemoteProvisioningState state, String message) {
-//                if (progress != null) progress.dismiss();
-//                if (state == LinphoneCore.RemoteProvisioningState.ConfiguringSuccessful) {
-//                    goToLinphoneActivity();
-//                } else if (state == LinphoneCore.RemoteProvisioningState.ConfiguringFailed) {
-//                    Toast.makeText(AssistantActivity.instance(), getString(R.string.remote_provisioning_failure), Toast.LENGTH_LONG).show();
-//                }
-//            }
-//
-//            @Override
-//            public void registrationState(LinphoneCore lc, LinphoneProxyConfig cfg, LinphoneCore.RegistrationState state, String smessage) {
-//                if (remoteProvisioningInProgress) {
-//                    if (progress != null) progress.dismiss();
-//                    if (state == LinphoneCore.RegistrationState.RegistrationOk) {
-//                        remoteProvisioningInProgress = false;
-//                        success();
-//                    }
-//                } else if (accountCreated && !newAccount){
-//                    if (address != null && address.asString().equals(cfg.getAddress().asString()) ) {
-//                        if (state == LinphoneCore.RegistrationState.RegistrationOk) {
-//                            if (progress != null) progress.dismiss();
-//                            if (LinphoneManager.getLc().getDefaultProxyConfig() != null) {
-//                                accountCreator.isAccountUsed();
-//                            }
-//                        } else if (state == LinphoneCore.RegistrationState.RegistrationFailed) {
-//                            if (progress != null) progress.dismiss();
-//                            if (dialog == null || !dialog.isShowing()) {
-//                                dialog = createErrorDialog(cfg, smessage);
-//                                dialog.show();
-//                            }
-//                        } else if(!(state == LinphoneCore.RegistrationState.RegistrationProgress)) {
-//                            if (progress != null) progress.dismiss();
-//                        }
+        textView = (TextView) findViewById(R.id.states_tv);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                answer();
+            }
+        });
+        mListener = new LinphoneCoreListenerBase(){
+            @Override
+            public void messageReceived(LinphoneCore lc, LinphoneChatRoom cr, LinphoneChatMessage message) {
+//                displayMissedChats(getUnreadMessageCount());
+            }
+
+            @Override
+            public void registrationState(LinphoneCore lc, LinphoneProxyConfig proxy, LinphoneCore.RegistrationState state, String smessage) {
+                if (state.equals(LinphoneCore.RegistrationState.RegistrationCleared)) {
+                    if (lc != null) {
+                        LinphoneAuthInfo authInfo = lc.findAuthInfo(proxy.getIdentity(), proxy.getRealm(), proxy.getDomain());
+                        if (authInfo != null)
+                            lc.removeAuthInfo(authInfo);
+                    }
+                }
+
+//                refreshAccounts();
+
+//                if(getResources().getBoolean(R.bool.use_phone_number_validation)) {
+//                    if (state.equals(LinphoneCore.RegistrationState.RegistrationOk)) {
+//                        LinphoneManager.getInstance().isAccountWithAlias();
 //                    }
 //                }
-//            }
-//        };
+
+//                if(state.equals(LinphoneCore.RegistrationState.RegistrationFailed) && newProxyConfig) {
+//                    newProxyConfig = false;
+//                    if (proxy.getError() == Reason.BadCredentials) {
+//                        //displayCustomToast(getString(R.string.error_bad_credentials), Toast.LENGTH_LONG);
+//                    }
+//                    if (proxy.getError() == Reason.Unauthorized) {
+//                        displayCustomToast(getString(R.string.error_unauthorized), Toast.LENGTH_LONG);
+//                    }
+//                    if (proxy.getError() == Reason.IOError) {
+//                        displayCustomToast(getString(R.string.error_io_error), Toast.LENGTH_LONG);
+//                    }
+//                }
+            }
+
+            @Override
+            public void callState(LinphoneCore lc, LinphoneCall call, LinphoneCall.State state, String message) {
+                if (state == LinphoneCall.State.IncomingReceived) {
+                    AppLog.d("收到来电");
+                    upState("收到来电");
+//                    startActivity(new Intent(LinphoneActivity.instance(), CallIncomingActivity.class));
+                    initLinphoneCall();
+                } else if (state == LinphoneCall.State.OutgoingInit || state == LinphoneCall.State.OutgoingProgress) {
+                    AppLog.d("state=OutgoingProgress");
+//                    startActivity(new Intent(LinphoneActivity.instance(), CallOutgoingActivity.class));
+                } else if (state == LinphoneCall.State.CallEnd || state == LinphoneCall.State.Error || state == LinphoneCall.State.CallReleased) {
+                    AppLog.d("state=CallEnd");
+//                    resetClassicMenuLayoutAndGoBackToCallIfStillRunning();
+                }
+
+                int missedCalls = LinphoneManager.getLc().getMissedCallsCount();
+                AppLog.d("MissedCallsCount丢失的数量="+missedCalls);
+//                displayMissedCalls(missedCalls);
+            }
+        };
+
         instance = this;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (!LinphoneService.isReady()) {
+            AppLog.w("LinphoneService掉了");
+            startService(new Intent(Intent.ACTION_MAIN).setClass(this, LinphoneService.class));
+        }
+
+        LinphoneCore lc = LinphoneManager.getLcIfManagerNotDestroyedOrNull();
+        if (lc != null) {
+            lc.addListener(mListener);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        LinphoneCore lc = LinphoneManager.getLcIfManagerNotDestroyedOrNull();
+        if (lc != null) {
+            lc.removeListener(mListener);
+        }
+        super.onPause();
     }
 
     private void initLogin() {
         AppLog.d("登录initLogin");
-        Bundle extras = new Bundle();
-        extras.putString("Phone", null);
-        extras.putString("Dialcode", null);
+//        Bundle extras = new Bundle();
+//        extras.putString("Phone", null);
+//        extras.putString("Dialcode", null);
         accountCreator = LinphoneCoreFactory.instance().createAccountCreator(LinphoneManager.getLc(), LinphonePreferences.instance().getXmlrpcUrl());
         accountCreator.setListener(new LinphoneAccountCreator.LinphoneAccountCreatorListener() {
             @Override
@@ -301,9 +365,87 @@ public class LinphoneActivity extends AppCompatActivity{
 //                displayRegistrationInProgressDialog();
 //            }
             accountCreated = true;
+            upState("用户已上线");
         } catch (LinphoneCoreException e) {
             Log.e(e);
             AppLog.e("LinphoneCoreException");
+        }
+    }
+
+    private void upState(final String state) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                if (textView!=null)
+                textView.setText(state);
+            }
+        });
+    }
+
+
+    private void initLinphoneCall() {
+        mCall = null;
+        // Only one call ringing at a time is allowed每次只能打一个电话
+        if (LinphoneManager.getLcIfManagerNotDestroyedOrNull() != null) {
+            List<LinphoneCall> calls = LinphoneUtils.getLinphoneCalls(LinphoneManager.getLc());
+            for (LinphoneCall call : calls) {
+                if (LinphoneCall.State.IncomingReceived == call.getState()) {
+                    mCall = call;
+                    AppLog.d("得到了call");
+                    break;
+                }
+            }
+        }
+        if (mCall == null) {
+            //The incoming call no longer exists.
+            Log.d("Couldn't find incoming call");
+            finish();
+            return;
+        }
+
+        LinphoneAddress address = mCall.getRemoteAddress();
+        LinphoneContact contact = ContactsManager.getInstance().findContactFromAddress(address);
+        if (contact != null) {
+//            LinphoneUtils.setImagePictureFromUri(this, contactPicture, contact.getPhotoUri(), contact.getThumbnailUri());
+//            name.setText(contact.getFullName());
+            AppLog.d("得到call name="+contact.getFullName());
+        } else {
+//            name.setText(LinphoneUtils.getAddressDisplayName(address));
+            AppLog.d("得到call elsename="+LinphoneUtils.getAddressDisplayName(address));
+        }
+//        number.setText(address.asStringUriOnly());
+        AppLog.d("得到call name="+address.asStringUriOnly());
+        textView.setText("来自"+contact.getFullName()+"的来电");
+    }
+    /**
+     * 接电话
+     */
+    private void answer() {
+        if (alreadyAcceptedOrDeniedCall) {
+            return;
+        }
+        alreadyAcceptedOrDeniedCall = true;
+
+        LinphoneCallParams params = LinphoneManager.getLc().createCallParams(mCall);
+
+        boolean isLowBandwidthConnection = !LinphoneUtils.isHighBandwidthConnection(LinphoneService.instance().getApplicationContext());
+
+        if (params != null) {
+            params.enableLowBandwidth(isLowBandwidthConnection);
+        }else {
+            Log.e("Could not create call params for call");
+        }
+
+        if (params == null || !LinphoneManager.getInstance().acceptCallWithParams(mCall, params)) {
+            // the above method takes care of Samsung Galaxy S
+            Toast.makeText(this, R.string.couldnt_accept_call, Toast.LENGTH_LONG).show();
+        } else {
+            if (!LinphoneActivity.isInstanciated()) {
+                return;
+            }
+            LinphoneManager.getInstance().routeAudioToReceiver();
+            upState("已接通，通话中");
+//            LinphoneActivity.instance().startIncallActivity(mCall);
         }
     }
 }
